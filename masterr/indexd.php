fb4a2e6df0d5605e522f578d123546be141037ec
26080 <144050431+26080@users.noreply.github.com>
2024-09-19 19:17:17 -0600

    php
    se subio la pagina conectada a la base de datos mediante php

---
+++ masterr/indexd.php
@@ -0,0 +1,132 @@
+<?php
+include'includes/conecta.php';
+include'includes/acciones.php';
+include'includes/querys.php'
+?>
+
+
+<!doctype html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Motos</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="css\estilos.css">
+</head>
+
+<body>
+  <header>
+    
+  </header>
+  <section class="carousel">
+    <div id="carouselExampleCaptions" class="carousel slide">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+          aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+          aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+          aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="https://2.bp.blogspot.com/-Mx85OKrTKz8/TlV8k73gZWI/AAAAAAAAABk/sSAlKZvdlWs/s1600/Mejores+motos+deportivas+1.jpg" class="d-block w-100" class="img-fluid" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h2>Motos</h2>
+            <p>Las motos, o motocicletas, son vehículos de dos ruedas que se propulsan mediante un motor..</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="https://www.moto1pro.com/sites/default/files/bmw_k_1600_gtl.jpg" class="d-block w-100" class="img-fluid" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h2>Motos de turismo</h2>
+            <p>Especialmente diseñadas para viajes largos, con características como asientos cómodos y espacio para equipaje..</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="https://th.bing.com/th/id/R.0fb975ff33ac3107bc5d8ff478194daa?rik=mHpnxlBzUhmnWA&riu=http%3a%2f%2fwww.moto1pro.com%2fsites%2fdefault%2ffiles%2fyamaha_yzf-r125_2019-1.jpg&ehk=1ewspJ6gZBI4Isk%2foUL5ScxtiQYyJ0V6GZOj1yIkFdI%3d&risl=&pid=ImgRaw&r=0" class="d-block w-100"
+            class="img-fluid" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h2>Moto deportiva</h2>
+            
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </section>
+  <section class="music my-5">
+    <h2 class="text-center my-5">¿Qué es una moto?</h2>
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <p>
+            Las motocicletas, comúnmente conocidas como motos, son vehículos de motor de dos ruedas que se diseñan para el transporte de una o dos personas. Se caracterizan por su estructura ligera y su capacidad para moverse rápidamente, lo que las hace ideales para la circulación en entornos urbanos y en carreteras..
+          </p>
+        </div>
+        <div class="col-6">
+          <img class="img-fluid" src="https://i.pinimg.com/736x/a8/53/2c/a8532c1d61e89962b2512b09d04a6040--ninja-zxr-kawasaki-ninja.jpg"
+            alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="music my-5">
+    <h2 class="text-center my-5">¿Como surge?</h2>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <img src="https://static.retail.autofact.cl/blog/c_url_original.56i9w1kfr1beaw.jpg" class="card-img-top"
+              alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Origen Prehistorico</h5>
+              <p class="card-text text-center">Orígenes (finales del siglo XIX):
+                <br>
+                Bicicletas: En la década de 1860, las bicicletas de pedales comenzaron a popularizarse, creando una base para el diseño de motocicletas. <br>
+                Motor de combustión: En 1885, el inventor alemán Gottlieb Daimler creó la "Reitwagen", considerada la primera motocicleta, que utilizaba un motor de combustión interna.
+                <br><br>
+                2. Primeras Producciones (principios del siglo XX): <br>
+                Producción en masa: En 1901, la compañía estadounidense Indian Motorcycle lanzó su primera motocicleta, marcando el inicio de la producción en masa. <br>
+                Crecimiento del mercado: Otras marcas, como Harley-Davidson, también comenzaron a producir motos, estableciendo una industria creciente.
+                <br><br>
+                3. Desarrollo y Diversificación (décadas de 1920 a 1950): <br>
+                Variación de estilos: Durante estas décadas, se desarrollaron diferentes tipos de motocicletas, como las de turismo y las deportivas. <br>
+                Competencias: Las carreras de motos ganaron popularidad, impulsando la innovación y el desarrollo tecnológico.
+                <br><br>
+                4. Expansión Global (décadas de 1960 a 1980): <br>
+                Marcas internacionales: Marcas japonesas como Honda, Yamaha y Suzuki comenzaron a dominar el mercado mundial, introduciendo modelos más accesibles y eficientes. <br>
+                Cultura motociclista: La cultura de las motocicletas se convirtió en un símbolo de libertad y rebeldía, especialmente en la década de 1960.
+                <br><br>
+                5. Innovaciones Tecnológicas (décadas de 1990 a la actualidad): <br>
+                Avances en diseño: La tecnología ha permitido la creación de motos más ligeras, potentes y eficientes en combustible. <br>
+                Motos eléctricas: En los últimos años, ha habido un aumento en la producción de motocicletas eléctricas, impulsadas por la necesidad de opciones más sostenibles.</p>
+              <a href="https://www.motor.es/que-es/motocicleta" class="btn btn-primary text-center">Ver Más</a>
+            </div>
+          </div>
+        </div>
+        
+
+    </div>
+    </div>
+    </div>
+  </section>
+ 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
